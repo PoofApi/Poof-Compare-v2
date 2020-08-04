154728e9fea6dbe303a8b5ccd2dbac59ec407bc3
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-08-04 17:42:32 -0400

    footer and css revisions for v2 mobile homepage

--- src/containers/MobileHeader.js
+++ src/containers/MobileHeader.js
@@ -19,60 +19,64 @@ class MobileHeader extends Component {
     render(){
 
         return(
-            <div className="container poofMobileContainer">
-                <div className="row">
-                    <div className="col-6">
-                        <div className="poofMobileLogo2">
-                            <img className="img-fluid" src={Poof_White} alt="poofMobileLogo2"/>
-                        </div>
-                    </div>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="col-9">
-                        <div className="poofMobileSearch">
-                            <input className="poofMobileInput" type="text" placeholder="Search for products..." name="search"/>
-                            <i className="material-icons poofMobileSearchIcon">search</i>
-                        </div>
-                    </div>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="col-5">
-                        <div className="icon1">
-                            <div className="poofElectronics">
-                                <img className="img-fluid poofLaptopIcon2" src={laptop} alt="poofLaptopIcon2"/>
-                                <div>Electronics</div>
+            <div className="poofMobileComponent">
+                <div className="container poofMobileContainer">
+                    <div className="row">
+                        <div className="col-7 mb-2">
+                            <div className="poofMobileLogo2">
+                                <img className="img-fluid" src={Poof_White} alt="poofMobileLogo2"/>
                             </div>
                         </div>
                     </div>
-                    <div className="col-5">                       
-                        <div className="poofBooks">
-                            <div className="icon2">
-                                <img className="img-fluid poofStudyIcon2" src={study} alt="poofStudyIcon2"/>
-                                <div>Books</div>
+                    <div className="row justify-content-center">
+                        <div className="col-9">
+                            <div className="poofMobileSearch mb-3">
+                                <input className="poofMobileInput" type="text" placeholder="Search for products..." name="search"/>
+                                <i className="material-icons poofMobileSearchIcon">search</i>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div className="row justify-content-center">
-                    <div className="col-5">                       
-                        <div className="poofClothes">
-                            <div className="icon3">
-                                <img className="img-fluid poofFashionIcon2" src={fashion} alt="poofFashionIcon2"/>
-                                <div>Electronics</div>
+                    <div className="row justify-content-center">
+                        <div className="col-5">
+                            <div className="icon1">
+                                <div className="poofElectronics">
+                                    <img className="img-fluid poofLaptopIcon2" src={laptop} alt="poofLaptopIcon2"/>
+                                    <div>Electronics</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-5">                       
+                            <div className="poofBooks">
+                                <div className="icon2">
+                                    <img className="img-fluid poofStudyIcon2" src={study} alt="poofStudyIcon2"/>
+                                    <div>Books</div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-5">
-                        <div className="poofGames">
-                            <div className="icon4">
-                                <img className="img-fluid poofGamingIcon2" src={gaming} alt="poofGamingIcon2"/>
-                                <div>Electronics</div>
+                    <div className="row justify-content-center">
+                        <div className="col-5">                       
+                            <div className="poofClothes">
+                                <div className="icon3">
+                                    <img className="img-fluid poofFashionIcon2" src={fashion} alt="poofFashionIcon2"/>
+                                    <div>Electronics</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-5">
+                            <div className="poofGames">
+                                <div className="icon4">
+                                    <img className="img-fluid poofGamingIcon2" src={gaming} alt="poofGamingIcon2"/>
+                                    <div>Electronics</div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="poofMobileFooter">
-                    
+                    <ul>
+                        <li></li>
+                    </ul>
                 </div>
             </div>
         )
