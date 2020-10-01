253b2154b3df79394c6f2b48575528f4d7fa9f42
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-10-01 17:34:14 -0400

    compare table scroll auto

--- src/components/Compare/index.js
+++ src/components/Compare/index.js
@@ -3,37 +3,42 @@ import './highlightCSS.css';
 import uuid from 'react-uuid';
 import { Link } from 'react-router-dom';
 
-const Compare = ({items, toggleClick}) =>
-  <div className="highlightContainer">
-    <div className="hToolbar">
-        <i className="material-icons toggleButton" onClick={() => toggleClick()} >expand_more</i>
-        <div className="detailsTitle">Details</div>
-        <div className="compareLinkHolder">
-          <Link className="compareRoute" to={'/compare-table'}><i className="material-icons">view_list</i></Link>
-        </div>
-    </div>
-    <div className="compareTableContainer">
-        {items.map(item =>
-            <div className="watchContainer compareContainer" key={uuid()}>
-              <div className="row">
-                <div className="col-3 col-md-3 comparePicHolder">
-                  <img className="img-fluid compareImage" src={item.image} alt={item.title} key={item.id}/>
-                </div>
-                <div className="col-5 col-md-8 info">
-                  <div className="itemName">
-                      <h3 className='username itemName2'>{item.title}</h3>
+const Compare = ({items, toggleClick}) => {
+
+  let amazon = [];
+  let ebay = [];
+  let bestBuy = [];
+
+  
+  return(
+    <div className="highlightContainer">
+      <div className="hToolbar">
+          <i className="material-icons toggleButton" onClick={() => toggleClick()} >expand_more</i>
+          <div className="detailsTitle">Poof! Compare Table</div>
+          <div className="compareLinkHolder">
+            <Link className="compareRoute" to={'/compare-table'}><i className="material-icons">view_list</i></Link>
+          </div>
+      </div>
+      <div className="compareTableContainer">
+              <div className="container-fluid container-scroll watchContainer compareContainer" key={uuid()}>
+                <div className="row">
+          {items.map(item =>
+                  <div className="col-md-2">
+                    <div className="card poofCompareCard">
+                      <div className="row">
+                        <div className="col-md-4">
+                          <img src={item.image} alt={item.title} key={item.id} style={{width: "50px", height: "70px"}}/>
+                        </div>
+                      </div>
+                    </div>
                   </div>
+          )}
                 </div>
               </div>
-              <div className="row">
-                <div className="col-4 priceTag">{`$${item.price}`}</div>
-                <div className="col-5 col-md-4 itemSource">
-                  <a href={`${item.link}`} target="_blank" className="compareSourceLogo"><img className={"img-fluid" + (item.logo == "https://firebasestorage.googleapis.com/v0/b/poofapibackend.appspot.com/o/icons%2Fbestbuy.png?alt=media&token=da783e57-802f-4a40-b609-3af1fd78098a" ? " compareBestBuyLogo" : " compareSourceLogoPic")} src={item.logo} alt={item.title}/></a>                
-                </div>
-              </div>
-            </div>
-        )}
+      </div>
     </div>
-  </div>;
+  )
+
+};
 
 export default Compare
