b2ce1bce3517da4a3208d129bd20b7ef7ddc8448
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-10-06 00:54:27 -0400

    Large update
    fullscreen mobile mix (Div100vh npm i), mobile css compare table update, topscroller logic update

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -6,6 +6,7 @@ import WatchlistRoute from '../WatchlistRoute.js';
 import CompareTableRoute from '../CompareTableRoute.js';
 import "materialize-css/dist/css/materialize.min.css";
 import "materialize-css/dist/js/materialize.min.js";
+import Div100vh from 'react-div-100vh';
 
 
 class App extends Component {
@@ -13,16 +14,18 @@ class App extends Component {
   render() {
 
     return (
-      <div className="app" style={{height: "100vh"}}>
-        <div className="container-fluid" style={{height: "100vh"}}>
-          <Switch>
-            <Route exact path="/" component={Home}/>
-            <Route exact path="/watchlist" component={WatchlistRoute} />
-            <Route exact path="/compare-table" component={CompareTableRoute} />
-            <Route component={NotFound}/>
-          </Switch>
+      <Div100vh>
+        <div className="app" style={{height: "100vh"}}>
+          <div className="container-fluid" style={{height: "100vh"}}>
+            <Switch>
+              <Route exact path="/" component={Home}/>
+              <Route exact path="/watchlist" component={WatchlistRoute} />
+              <Route exact path="/compare-table" component={CompareTableRoute} />
+              <Route component={NotFound}/>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Div100vh>
     )
   }
 }
