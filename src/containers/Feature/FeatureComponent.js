e513e624b799452a5c37bca0992f07206264ce58
Greg Cruz D.O <49351487+x1jinchu1x@users.noreply.github.com>
2020-11-06 22:31:59 -0500

    features route complete

--- src/containers/Feature/FeatureComponent.js
+++ src/containers/Feature/FeatureComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import HeaderComponent2 from '../HeaderComponent2'
 import { withRouter } from 'react-router-dom'
-import FooterComponentSearchPage from '../FooterComponentSearchPage';
+import FooterComponentSearchPage from '../FooterComponentSearchPage'
 import './featureStyle.css'
 
 
@@ -50,16 +50,43 @@ class FeatureComponent extends Component {
                     <div className="col-6">
                         <img className="img-fluid iphonePic1" src="https://scrapping-logos.s3.amazonaws.com/V1/Iphone+2.png" alt="poofPhonePic"/>
                     </div>
-                    <div className="col-5 iphone1Text">
-                        Search and compare items from major big box retailers!
+                    <div className="col-5 iphone1TextBody">
+                        <div className="iphone1Text">
+                            Search and Compare
+                        </div>
+                        <div>
+                            With just one search, you can view and compare items from big box retailers, all in one tab!
+                        </div>
                     </div>
                 </div>
                 <div className="row align-items-center iphonePic2Row">
-                    <div className="col-5 iphone2Text">
-                        Not sure you want to make a purchase just yet? Simply add it to your watchlist!
+                    <div className="col-5 iphone2TextBody">
+                        <div className="iphone2Text">
+                            Watchlist!
+                        </div>
+                        <div>
+                            Not so sure you want to make a purchase just yet? Simply add it to your watchlist for later!
+                        </div>
                     </div>
                     <div className="col-6">
                         <img className="img-fluid iphonePic2" src="https://scrapping-logos.s3.amazonaws.com/V1/Iphone+1.png" alt="poofPhonePic2"/>
+                    </div>
+                </div>
+                <div className="gifInstruction">
+                    <div className="row justify-content-center">
+                        <div className="col-6">
+                            <div className="gifInstructionText">
+                                Compare and Buy Smart!
+                            </div>
+                            <div className="gifInstructionTextBody mt-4">
+                                As demonstrated in the video below, after performing a search, go ahead and add or remove items to your Poof! Compare Table to assist you in making your smart purchases!
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center gifInstructionVid">
+                        <div className="col-6">
+                            <img className="img-fluid" src="https://scrapping-logos.s3.amazonaws.com/V1/featuresGif.gif" alt="featureGifInstruction"/>
+                        </div>
                     </div>
                 </div>
             </div>
